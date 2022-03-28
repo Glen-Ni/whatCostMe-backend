78d8e286c1f16bbde98b60ec6cc931eb103ff437
小僵尸RUA <nijialun888@hotmail.com>
2022-03-28 15:15:30 +0800

    实现路由create show方法

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def show
+    user = User.find_by_id params[:id]
+    if user
+      render json: user
+    else
+      head 404
+    end
+  end
+
+  def create
+    user= User.new name: 'sb'
+    user.save
+    if user.save
+      render json: user
+    else 
+      render json: user.errors
+    end
+  end
+end
